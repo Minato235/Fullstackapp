1137f68a0116f2df6c4747293b072cf295f4f8f7
Minato <ankamasish@gmail.com>
2022-11-11 09:57:59 +0530

    added delte and get

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const bodyParser = require('body-parser')
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -7,8 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const User = require('./models/User');
-const sequelize = require("./utill/database")
-const port = 4000;
+// const User2 = require('./models/User2');
+
+const sequelize = require("./utill/database");
+const { isErrored } = require('stream');
 
 
 app.post("/user/addUser", async (req, res, next) => {
@@ -26,11 +27,53 @@ app.post("/user/addUser", async (req, res, next) => {
   });
 });
 
+app.get("/user/getUser",async (req,res,next)=>{
+  try{
+  const users=await User.findAll();
+  res.status(200).json(users);
+  }catch(err){
+    console.log('err getting');
+    res.status(500).json({err:err})
+  }
+})
+
+app.delete("/user/deleteUser/:id",async (req,res,next)=>{
+  
+  try{
+  let userId=req.params.id;
+  if(!userId){
+    res.status(400).json({err:'Id missing!!'})
+  }
+  await User.destroy({where:{id:userId}});
+  console.log('delete working');
+
+  res.sendStatus(200);
+}
+catch(err){
+  console.log(err)
+  res.status(500).json({err:'isErrored deletete section'})
+}
+})
+//dummy
+// app.post("/user/addUser", async (req, res, next) => {
+//   const name = req.body.name;
+//   const email = req.body.email;
+//   const phoneNo = req.body.phoneNo;
+
+//   const data = await User2.create({
+//     name: name,
+//     email: email,
+//     phoneNo: phoneNo
+//   });
+//   res.status(201).json({
+//     newUserDetail: data
+//   });
+// });
 
 
 sequelize.sync()
 .then(()=>{
-  app.listen(port)
+  app.listen(4000)
 })
 .catch((err)=>{
   console.log(err);
---
+++ models/User2.js
@@ -0,0 +1,30 @@
+const Sequelize=require("sequelize");
+const sequelize=require("../utill/database");
+const DataTypes = require('mysql2');
+
+const User2 = sequelize.define('new', {
+    id:{
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+
+    },
+    name:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email:{
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull:false
+    },
+    phoneNo: {
+        type: Sequelize.STRING,
+        // unique: true,
+        // allowNull:false
+    }
+});
+
+
+module.exports=User2;
